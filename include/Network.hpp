b6da5aff4c3a2658976a9adea152a3f3c9be4dbf
Léo Sumi <leo.sumi@epfl.ch>
2016-10-16 00:07:26 +0200

    Merge include/Network.hpp and src/network.hpp together

--- include/Network.hpp
+++ include/Network.hpp
@@ -13,7 +13,12 @@ public:
 	Network(/* arguments */);
 	virtual ~Network();
 
+	void update(/* arguments */);
+
 private:
+	int N_; /// total number of neurons
+	int Ne_; /// number of excitatory neurons
+	int Ni_; /// number of inhibitatory neurons
 	std::vector<Neuron> neurons_;
 	std::vector<Connection> connections_from_surroundings_;
 };

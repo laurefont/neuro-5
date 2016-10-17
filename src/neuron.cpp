345dcd8e98cccc2b78793d72d24734be32eafb9f
laurinekolly <laurine.kolly@epfl.ch>
2016-10-17 14:24:46 +0200

    merged neuron hpp files from Fiona and Laurine

--- src/neuron.cpp
+++ src/neuron.cpp
@@ -1,12 +1,7 @@
-#include "neuron.hpp"
+#include "Neuron.hpp"
 
 
-Neuron::Neuron() : teta(20), tau_rp(2)
-{}
-
-void Neuron::set_thresh_f()
+Neuron::Neuron(bool const& exc, double const& eps, double const& tau, double const& ext_f) : excitatory_(exc), inhib_connections_(250), excit_connections_(1000), epsilon_(eps), tau_(20), ext_f_(ext_f), firing_threshold_(20), rest_potential_(10),transmission_delay_(1.5), refactory_period_(2), amplitude_(0.1)
 {
-	thresh_f = teta / (J * excit_connexions * tau);
 }
-
 

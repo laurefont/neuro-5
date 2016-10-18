692520f9312634d0395ef24dd9c4b2436c5c4859
Julie Brancato <julie.brancato@epfl.ch>
2016-10-18 21:51:15 +0200

    Creation of index.cpp to add things to the main page

---
+++ src/index.cpp
@@ -0,0 +1,13 @@
+/*! \mainpage My Personal Index Page
+ *
+ * \section install_sec Section 1
+ *
+ * This is the section 1.
+ *
+ * \section install_sec Section 2
+ *
+ * \subsection Subsection 2.1
+ * \subsection Subsection 2.2
+ *
+ * etc...
+ */

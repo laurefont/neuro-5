9bef8f99424f59fe45fffa259096fed451337dd7
Julie Brancato <julie.brancato@epfl.ch>
2016-10-24 14:16:46 +0200

    Add Event.cpp, change Event.hpp

--- include/Event.hpp
+++ include/Event.hpp
@@ -1,20 +1,23 @@
-#pragma once
 
-class CurrentEvent
-{
+#ifndef EVENT_H
+#define EVENT_H
 
-  public:
-    Event(double const& time, double const& current);
-    ~Event();
-    bool operator<(Event const&, Event const&) const;
-    double get_i() const;
-    double get_t() const;
-    
-    private:
-    
-    double t_;
-    double i_;
-    
+class Event {
+
+	public:
+	
+	Event(double const& time, double const& current);
+	~Event();
+	bool operator<(Event const&, Event const&) const;
+	double get_i() const;
+	
+	private:
+	
+	double t_; ///> time
+	double i_; ///> current
+	
+
 };
+
 
 #endif

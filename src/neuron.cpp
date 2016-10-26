e993bad78d681041a277b91e51389af4fb4b1fb6
laurinekolly <laurine.kolly@epfl.ch>
2016-10-26 21:59:26 +0200

    modified and adapted neuron.cpp + added comments that need to be transfered to hpp in doxygen approprate format

--- src/neuron.cpp
+++ src/neuron.cpp
@@ -8,95 +8,205 @@ Physics::Resistance const Neuron::membrane_resistance_= 5; ///< nbre qulconque
 Physics::Amplitude const Neuron::amplitude_= 0.1;
 
 
-Neuron::Neuron(bool const& exc, double const& eps, double const& tau, double const& ext_f, double const& time) : excitatory_(exc), inhib_connections_(250), excit_connections_(1000), epsilon_(eps), tau_(20), ext_f_(ext_f), t_(0)
+
+
+
+Neuron::Neuron(bool const& exc, double const& eps, double const& ext_f)
+: excitatory_(exc), inhib_connections_(250), excit_connections_(1000), epsilon_(eps), tau_(20), ext_f_(ext_f), t_(0)
 {
-	synapses_ = std::vector<Neuron*>(1250);
+    synapses_ = std::vector<Neuron*>(1250);
+    std::priority_queue <Event> ev;
+    events_in_ = ev;
+    events_out_ = ev;
+    // on initialise events_in_ et events_out_ à un tableau vide
 }
+
+
+
+
 
 Neuron::~Neuron()
 {
-	for (auto& element : synapses_) {
-		delete element;
-		element = nullptr;
-	}
-	synapses_.clear();
+    for (auto& element : synapses_) {
+        delete element;
+        element = nullptr;
+    }
+    synapses_.clear();
 }
-	
-	
-void Neuron::add_event(double const& a_time, double const& a_current)
-{
+
+
+
+
+
+// En fait je me rends compte que cette fonction sert à rien vu qu'on peut direct push
+/*
+ void Neuron::add_event(double const& a_time, double const& a_current)
+ {
 	Event e1(a_time, a_current);
-	events_.push(e1);
+	events_in_.push(e1);
+ }
+ 
+ */
+
+
+
+
+void Neuron::input(double const& dt)
+{
+    
+    int nb_of_inputs (0);
+    
+    for (size_t i(0); i < synapses_.size(); ++i)
+    {
+        double X = t_ - synapses_[i]->get_t_output() - transmission_delay_; // ce qui nous intéresse c'est le temps auquel
+        // l'autre neurone a output, puisqu'on regarde
+        //si deltaT = D (transmission_delay_) et si c'est le cas I = J
+        
+        
+        if(X == 0)    // Si X = 0, alors dirac(X) = 1 --> I =J
+        {
+            Event ev(t_, amplitude_);
+            events_in_.push(ev);
+            synapses_[i]->clear_top_output(); // indique que l'output de la synapse[i] a été utilisé donc doit
+            // être sorti du tableau d'events_out_ de la synapse en question
+            
+            ++nb_of_inputs;
+        }
+        
+    }
+    
+    Vm_ += (nb_of_inputs * amplitude_) / membrane_resistance_;
+    
+    // ici temporaire :creer une fonction qui met a jour la valeur du potentiel membranaire et l'appeler ici
+    
+    
 }
 
 
-void Neuron::input()
+
+
+
+
+
+
+// additionne tous les courants contenus dans les éléments de events_in_ ayant un t < t_ (notre attribut)
+// pour ce faire on delete tous ces éléments un par un, après avoir incrémenté la valeur de leur courant
+// à notre total (sum), qui va être retourné par la fonction en question
+double Neuron::sum_events(double const& dt)
 {
-	for (size_t i(0); i < synapses_.size(); ++i)
-	{
-		double I_added(0);
-		I_added = synapses_[i]->output() / membrane_resistance_; ///< en considérant que output est un V
-		add_event(t_, I_added);
-	}
+    double sum(0);
+    
+    while(events_in_.top().get_t() < (t_ + dt))
+    {
+        sum += events_in_.top().get_i();
+        events_in_.pop();
+    }
+    
+    return sum;
 }
 
 
-double Neuron::sum_events(double const& dt) const
+
+
+
+
+
+//Je trouve aussi cette méthode assez inutile vu qu'on pourrait juste invoquer tout ça dans update, mais à voir
+void Neuron::output(double const& x)
 {
-	double sum(0);
+    Event ev(t_, x);
+    events_out_.push(ev);
+}
+
+
+
+
+
+// Cette méthode est aussi inutile car c'est bcp plus simple de faire ça dans update
+/*
+ void Neuron::spike()
+ {
 	
-	while(events_.top().get_t() < (t_ + dt))
-	{
-		sum += events_.top().get_i();
-		events_.pop();
-	}
-	
-	return sum;
-}
-
-
-double Neuron::output(double const& dt) const
-{
 	if (has_reached_threshold())
 	{
-		return sum_events(dt); 
-	} else {
-		return 0;
+ reset_potential();
 	}
-}
+ }
+ */
 
-		
-void Neuron::spike()
-{
-	if (has_reached_threshold())
-	{
-		reset_potential();
-		///< empty events
-	}
-}
-		
+
+
+
+
 
 
 bool Neuron::has_reached_threshold() const
 {
-	return sum_events() >= firing_threshold_;
+    return Vm_ >= firing_threshold_;
 }
 
 
+
+
+
+
+
+
+// remet le potentiel de membrane au potentiel au repos, cette méthode sera appelée
+// dans update si le threshold est dépassé
 void Neuron::reset_potential()
-{ 
-	V_ = rest_potential_;
+{
+    Vm_ = rest_potential_;
 }
+
+
+
+
+
+
+
 
 
 void Neuron::update(double const& dt)
 {
+    
+    input(dt); //met d'abord à jour les input
+    
+    /*
+     double X = sum_events(dt); // petit problème ici :on va perdre la somme X
+     // dans laquelle sont stockés tous les éléments de events_in_
+     // (parce qu'en faisant sum_events on vide aussi le tableau)
+     
+     */
+    
+    
+    
+    // puis met à jour les output dans le cas où le threshold est atteint
+    if (has_reached_threshold())
+    {
+        //output(X);
+        output(sum_events(dt));
+        reset_potential();
+    }
+    
 }
 
-			
-		
-	
-	
+// retourne le temps de l'output le plus ancien
+double Neuron::get_t_output() const
+{
+    return events_out_.top().get_t();
+}
+
+
+
+//enlève l'output le plus ancien de events_out_ (à appeler que dans le cas où cet output a été utilisé)
+void Neuron::clear_top_output()
+{
+    events_out_.pop();
+}
+
+
+
 	
 	
 	

b3ffee495222f375569148104851dffe91241a62
laurefont <laure.font@epfl.ch>
2016-10-27 20:11:43 +0200

    commentaires attributs

--- include/Neuron.hpp
+++ include/Neuron.hpp
@@ -39,26 +39,26 @@ class Neuron {
     
     
     //attributs
-    bool  const excitatory_;
-    int const inhib_connections_; ///>number of connections from other inhibitatory neurons
-    int const excit_connections_; ///>number of connections from other excitatory neurons
-    double const epsilon_; ///>connection density
-    double I_; ///>synaptic currents arriving at the soma
-    double Vm_; ///>membrane potential
-    double  const tau_; ///>time constant of the circuit
-    double ext_f_; ///>external frequency
+    bool  const excitatory_; ///<true if neuron excitatory OR false if neuron inhibatory
+    int const inhib_connections_; ///<number of connections from other inhibitatory neurons
+    int const excit_connections_; ///<number of connections from other excitatory neurons
+    double const epsilon_; ///<connection density
+    double I_; ///<synaptic currents arriving at the soma
+    double Vm_; ///<membrane potential
+    double  const tau_; ///<time constant of the circuit
+    double ext_f_; ///<external frequency
     
-    double t_;
-    std::priority_queue <Event> events_in_;
-    std::priority_queue <Event> events_out_;
-    std::vector <Neuron*> synapses_;
+    double t_; ///<time
+    std::priority_queue <Event> events_in_; ///<queue of input events 
+    std::priority_queue <Event> events_out_; ///<queue of output events
+    std::vector <Neuron*> synapses_; ///<table with the neurons it's sending signals to
     
-    static const Physics::Potential firing_threshold_;
-    static const Physics::Potential rest_potential_; ///>Vr
-    static const Physics::Time transmission_delay_; ///>D
-    static const Physics::Time refactory_period_; ///>tau_rp
-    static const Physics::Resistance membrane_resistance_; ///>R
-    static const Physics::Amplitude amplitude_; ///>J
+    static const Physics::Potential firing_threshold_; ///<membrane potential level at which neuron fires
+    static const Physics::Potential rest_potential_; ///<Vr (reset potential after the neuron has fired)
+    static const Physics::Time transmission_delay_; ///D (time taken by a signal after it's been produced to reach the receiving neuron)
+    static const Physics::Time refactory_period_; ///<tau_rp (period after an output, during which neuron can't receive inputs and can't fire)
+    static const Physics::Resistance membrane_resistance_; ///<R (resistance of the membrane)
+    static const Physics::Amplitude amplitude_; ///<J (amplitude of an input)
     
     
 };

85b47d3016493102d867b6b241fd5a5121439efa
laurinekolly <laurine.kolly@epfl.ch>
2016-11-07 20:55:12 +0100

    adapted neuron to the different step methods

--- include/Neuron.hpp
+++ include/Neuron.hpp
@@ -7,20 +7,28 @@
 #include <queue>
 
 
+enum class Type : short
+{
+	Analytic, Explicit, Implicit
+};
+
+
 class Neuron {
     
     public :
     
     //constructeur et destructeur
-    Neuron(bool const& exc, double const& eps, double const& ext_f);
+    Neuron(int const& a_type, bool const& exc, double const& eps, double const& ext_f);
     ~Neuron();
     
     
     //méthode publique
     void update(Physics::Time const& dt);
     void set_connection(Neuron* neuron);
-    
-    
+    void step(Physics::Time const& dt); ///< à peut-être mettre en privé
+    void step_analytic(Physics::Time const& dt);
+    void step_explicit(Physics::Time const& dt);
+    void step_implicit(Physics::Time const& dt);
     
    
     private :
@@ -35,6 +43,7 @@ class Neuron {
     
     
     //attributs
+    Type type_;
     bool  const excitatory_; ///<true if neuron excitatory OR false if neuron inhibatory
     int const inhib_connections_; ///<number of connections from other inhibitatory neurons
     int const excit_connections_; ///<number of connections from other excitatory neurons
@@ -51,7 +60,7 @@ class Neuron {
     static const Physics::Potential firing_threshold_; ///<membrane potential level at which neuron fires
     static const Physics::Potential rest_potential_; ///<Vr (reset potential after the neuron has fired)
     static const Physics::Time transmission_delay_; ///<D (time taken by a signal after it's been produced to reach the receiving neuron)
-    static const Physics::Time refactory_period_; ///<tau_rp (period after an output, during which neuron can't receive inputs and can't fire)
+    static const Physics::Time refractory_period_; ///<tau_rp (period after an output, during which neuron can't receive inputs and can't fire)
     static const Physics::Resistance membrane_resistance_; ///<R (resistance of the membrane)
     static const Physics::Amplitude amplitude_; ///<J (amplitude of an input)
     

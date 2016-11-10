ca1260fe89c55f02c10fe3728885f63c1e059b1a
Léo Sumi <leo.sumi@epfl.ch>
2016-11-10 12:32:03 +0100

    Add simulation constructor, adapt network constructor

--- src/Network.cpp
+++ src/Network.cpp
@@ -6,13 +6,15 @@
 
 #include <Physics.hpp>
 
-Network::Network(unsigned int const number_neurons, double const gamma, double const epsilon, double const g)
+Network::Network(Type const type, unsigned int const number_neurons, double const gamma, double const epsilon, Physics::Frequency const ext_f, Physics::Resistance const membrane_resistance)
 	: N_(number_neurons),
 	  Ne_(std::round(N_ / (1 + gamma))),
 	  Ni_(N_ - Ne_),
 	  gamma_(gamma),
 	  epsilon_(epsilon),
-	  g_(g)
+	  ext_f_(ext_f),
+	  membrane_resistance_(membrane_resistance),
+	  type_(type)
 {
 }
 

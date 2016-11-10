6345d3cf503082333a6e723e0edbf1c0f72c409e
laurinekolly <laurine.kolly@epfl.ch>
2016-11-10 11:10:01 +0100

    worked on implicit step and simulation with Léo and Thaïs

--- include/Simulation.hpp
+++ include/Simulation.hpp
@@ -3,6 +3,7 @@
 
 #include <Network.hpp>
 #include <Plot.hpp>
+#include <Physics.hpp>
 
 /**
  * @brief The main class of the simulation
@@ -12,12 +13,18 @@
 class Simulation
 {
     public:
-        Simulation(/* arguments */);
+		/// in case of analytic no argument needed -> time stepping is not in option, it will be 1 by default
+        Simulation(Physics:Time const& time_of_simulation); 
+        Simulation(Physics:Time const& time_of_simulation, Type const& type,
+					Physics:Time const& time_step,
+					Physics::Resistance const& resistance);
         virtual ~Simulation();
+        void launch_simulation();
 
     private:
         Network network_;
         Plot plot_;
+        Physics::Time time_of_simulation_;
 };
 
 #endif // SIMULATION_HPP
---
+++ src/Simulation.cpp
@@ -0,0 +1,21 @@
+#include "Simulation.hpp"
+
+
+
+Simulation::Simulation(Physics:Time const& time_of_simulation) 
+{
+	
+}
+ 
+Simulation(Physics:Time const& time_of_simulation, Type const& type,
+			Physics:Time const& time_step,
+			Physics::Resistance const& resistance)
+{
+	
+}
+
+void Simulation::launch_simulation()
+{
+	
+}
+

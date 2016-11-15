9db5631ff18e63ce06a237ce28a1855f422b6d63
Gael Reganha <gael.reganha@epfl.ch>
2016-11-15 15:17:31 +0100

    MAJ tests for each type

--- test/ExplicitNeuronTest.cpp
+++ test/ExplicitNeuronTest.cpp
@@ -2,7 +2,8 @@
 #include <gtest/gtest.h>
 #include "Simulation.hpp"
 
-TEST(ImplicitNeuronTests, TestFiring)
+
+TEST(ExplicitNeuronTests, TestFiring)
 {
     Neuron neurone(Type::Explicit, true, 1, 1, 3);
     neurone.set_Vm_ (7);
@@ -15,34 +16,21 @@ TEST(ImplicitNeuronTests, TestFiring)
     EXPECT_TRUE(abs(neurone.get_Vm_() - 7.4) < 0.000001);
 }
 
-TEST(ExplicitNeuronTests, TestFiring)
+TEST(ExplicitNeuronTests, TestStepExplicit)
 {
     Neuron neurone(Type::Explicit, true, 1, 1, 3);
-    neurone.set_Vm_ (7); 
-    neurone.set_I_(5); 
-    int dt = 1; 
+    neurone.set_Vm_ (7);
+    neurone.set_I_(5);
+    int dt = 1;
+
     neurone.Neuron::step_explicit(1);
+
     int result = neurone.get_Vm_();
-    
+
     EXPECT_NEAR (7.4, neurone.get_Vm_(),  0.000001);
     EXPECT_TRUE(abs(neurone.get_Vm_() - 7.4) < 0.000001);
 }
 
-TEST(ExplicitNeuronTests, TestStepExplicit)
-{
-    Neuron neurone(Type::Explicit, true, 1, 1, 3);
-    neurone.set_Vm_ (7); 
-    neurone.set_I_(5); 
-    int dt = 1; 
-    
-    neurone.Neuron::step_explicit(1);
-    
-    int result = neurone.get_Vm_();
-       
-    EXPECT_NEAR (7.4, neurone.get_Vm_(),  0.000001);
-    EXPECT_TRUE(abs(neurone.get_Vm_() - 7.4) < 0.000001);
-}
-  
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);

4ff2e4850a4a35de5776a93dccc3d2ff107d44ee
Fiona Joseph <fionajoseph@tsf-452-wpa-2-152.epfl.ch>
2016-12-06 14:51:41 +0100

    removed useless stuff in Simulation.hpp (Julie+Fiona)

--- src/Neuron.cpp
+++ src/Neuron.cpp
@@ -215,8 +215,8 @@ void Neuron::step_analytic(Physics::Time const& dt)
 
 
 void Neuron::step_explicit(Physics::Time const& dt)// Use of V(t-1)=Vm_ to calculate the new Vm_
-{
-    //if( t_ + dt >= last_spike_time_ + refractory_period_)
+{ 
+    if( t_ + dt >= last_spike_time_ + refractory_period_)
     {
        Vm_ += ((-Vm_ + RI(dt)) * dt) / tau_;
 	}
@@ -225,7 +225,7 @@ void Neuron::step_explicit(Physics::Time const& dt)// Use of V(t-1)=Vm_ to calcu
 
 void Neuron::step_implicit(Physics::Time const& dt)
 {
-    //if( t_ + dt >= last_spike_time_ + refractory_period_)
+    if( t_ + dt >= last_spike_time_ + refractory_period_)
 	{
 		Vm_ = ((dt * RI(dt) ) + (tau_ * Vm_)) / ( dt + tau_);
 	}

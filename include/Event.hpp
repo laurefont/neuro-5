188a77d589ca3c6070a0c4ac1b08d7fe16e33c40
Léo Sumi <leo.sumi@epfl.ch>
2016-12-16 16:27:00 +0100

    doc event class, autotab and autoindent event.hpp

--- include/Event.hpp
+++ include/Event.hpp
@@ -3,40 +3,54 @@
 
 #include "Physics.hpp"
 
-class Event {
+///
+/// @brief Neuron state at a definite time
+///
+/// Event is used to store neurons state in memory.
+/// A state is represented by a time point in the simulation and a current intensity.
+///
+class Event 
+{
+public:
+    /// 
+    /// Constructor
+    ///
+    /// Creates an output to send to a receiving neuron
+    ///
+    /// @param time time at which the signal reaches the receiving neuron
+    /// @param weight_J intensity of the signal
+    ///
+    Event(Physics::Time const& time, Physics::Amplitude const& weight_J);
+    
+    ///
+    /// @brief Destructor
+    ///
+    ~Event();
 
-	public:
-	
-	/// 
-	/// Creates an output to send to a receiving neuron
-	///
-	/// @param time the time at which the signal reaches the receiving neuron
-	/// @param current the intensity of the signal
-	///
-    Event(Physics::Time const& time, Physics::Potential const& weight_J);
-	
-
-	~Event(); ///<Destructor
-	
-	
-	///
-	/// Compares the times of 2 events to know which one happens first and so which one will be first in the queue
-	///
-	/// @param Event reference to another event with which we want to compare the time of occuring
-	///
-	bool operator<(Event const&) const;
-	
-    double get_J() const; ///<returns the intensity of the current of the signal
-	
-	double get_t() const; ///<returns the time of occuring of the signal
-	
-	private:
-	
-	Physics::Time t_; ///< time at which the neuron receives the input
-    double J_; ///< intensity of the current of the signal
-	
-
+    ///
+    /// Compares the times of 2 events to know which one happens first and so which one will be first in the queue
+    ///
+    /// @param Event reference to another event with which we want to compare the time of occuring
+    ///
+    bool operator<(Event const&) const;
+    
+    ///
+    /// @brief Get the intensity of the current of the signal
+    ///
+    /// @return current intensity
+    ///
+    double get_J() const;
+    
+    ///
+    /// @brief Get the time of the signal
+    ///
+    /// @return time of occurring signal
+    ///
+    double get_t() const;
+    
+private:
+    Physics::Time t_; ///< time at which the neuron receives the input
+    Physics::Amplitude J_; ///< intensity of the current of the signal
 };
 
-
-#endif
+#endif // EVENT_H

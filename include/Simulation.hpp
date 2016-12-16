341f33d17aa831dac9992823abdb63fe579faf19
Léo Sumi <leo.sumi@epfl.ch>
2016-12-16 12:16:20 +0100

    Remove get_simulation_time from interface, autotab, autoindent simulation.hpp

--- include/Simulation.hpp
+++ include/Simulation.hpp
@@ -13,82 +13,81 @@
  */
 class Simulation
 {
-    public:
-        ///
-        /// Create a fixed-step method simulation
-        ///
-        /// @param number_neurons number of neurons in the network
-        /// @param time_of_simulation duration of the simulation
-        /// @param time_step time step duration
-        /// @param type type of solution
-        /// @param gamma proportion of inhibitory neurons over excitatory neurons
-        /// @param epsilon connections density
-        /// @param external_factor external factor
-        ///
-        Simulation( unsigned int const number_neurons,
-                    Physics::Time const& time_of_simulation,
-                    Physics::Time const& time_step,
-                    SimulationType const& type,
-                    bool const& add_external_current = true,
-                    std::vector<unsigned int>* neuron_csv_files = NULL,
-                    Physics::Potential firing_threshold = FIRING_THRESHOLD,
-                    Physics::Time refractory_period = REFRACTORY_PERIOD,
-                    Physics::Potential resting_potential = RESTING_POTENTIAL,
-                    Physics::Potential reset_potential = RESET_POTENTIAL,
-                    Physics::Time transmission_delay = TRANSMISSION_DELAY,
-                    Physics::Time tau = TAU,
-                    double const gamma = GAMMA,
-                    double const epsilon = EPSILON,
-                    double const& external_factor = EXTERNAL_FACTOR,
-                    unsigned random_seed = RANDOM_SEED,
-                    Physics::Time spike_interval = SPIKE_INTERVAL );
+public:
+    ///
+    /// Create a fixed-step method simulation
+    ///
+    /// @param number_neurons number of neurons in the network
+    /// @param time_of_simulation duration of the simulation
+    /// @param time_step time step duration
+    /// @param type type of solution
+    /// @param gamma proportion of inhibitory neurons over excitatory neurons
+    /// @param epsilon connections density
+    /// @param external_factor external factor
+    ///
+    Simulation( unsigned int const number_neurons,
+                Physics::Time const& time_of_simulation,
+                Physics::Time const& time_step,
+                SimulationType const& type,
+                bool const& add_external_current = true,
+                std::vector<unsigned int>* neuron_csv_files = NULL,
+                Physics::Potential firing_threshold = FIRING_THRESHOLD,
+                Physics::Time refractory_period = REFRACTORY_PERIOD,
+                Physics::Potential resting_potential = RESTING_POTENTIAL,
+                Physics::Potential reset_potential = RESET_POTENTIAL,
+                Physics::Time transmission_delay = TRANSMISSION_DELAY,
+                Physics::Time tau = TAU,
+                double const gamma = GAMMA,
+                double const epsilon = EPSILON,
+                double const& external_factor = EXTERNAL_FACTOR,
+                unsigned random_seed = RANDOM_SEED,
+                Physics::Time spike_interval = SPIKE_INTERVAL );
 
+    ///
+    /// Create a simulation with an analytic variable-step solution
+    ///
+    /// @param number_neurons number of neurons in the network
+    /// @param time_of_simulation duration of the simulation
+    /// @param gamma proportion of inhibitory neurons over excitatory neurons
+    /// @param epsilon connections density
+    /// @param external_factor external factor
+    ///
+    Simulation( unsigned int const number_neurons,
+                Physics::Time const& time_of_simulation,
+                bool const& add_external_current = true,
+                std::vector<unsigned int>* neuron_csv_files = NULL,
+                Physics::Potential firing_threshold = FIRING_THRESHOLD,
+                Physics::Time refractory_period = REFRACTORY_PERIOD,
+                Physics::Potential resting_potential = RESTING_POTENTIAL,
+                Physics::Potential reset_potential = RESET_POTENTIAL,
+                Physics::Time transmission_delay = TRANSMISSION_DELAY,
+                Physics::Time tau = TAU,
+                double const gamma = GAMMA,
+                double const epsilon = EPSILON,
+                double const& external_factor = EXTERNAL_FACTOR,
+                unsigned random_seed = RANDOM_SEED,
+                Physics::Time spike_interval = SPIKE_INTERVAL );
 
-        ///
-        /// Create a simulation with an analytic variable-step solution
-        ///
-        /// @param number_neurons number of neurons in the network
-        /// @param time_of_simulation duration of the simulation
-        /// @param gamma proportion of inhibitory neurons over excitatory neurons
-        /// @param epsilon connections density
-        /// @param external_factor external factor
-		///
-        Simulation( unsigned int const number_neurons,
-                    Physics::Time const& time_of_simulation,
-                    bool const& add_external_current = true,
-                    std::vector<unsigned int>* neuron_csv_files = NULL,
-                    Physics::Potential firing_threshold = FIRING_THRESHOLD,
-                    Physics::Time refractory_period = REFRACTORY_PERIOD,
-                    Physics::Potential resting_potential = RESTING_POTENTIAL,
-                    Physics::Potential reset_potential = RESET_POTENTIAL,
-                    Physics::Time transmission_delay = TRANSMISSION_DELAY,
-                    Physics::Time tau = TAU,
-                    double const gamma = GAMMA,
-                    double const epsilon = EPSILON,
-                    double const& external_factor = EXTERNAL_FACTOR,
-                    unsigned random_seed = RANDOM_SEED,
-                    Physics::Time spike_interval = SPIKE_INTERVAL );
+    virtual ~Simulation();
+    void launch_simulation();
 
-        virtual ~Simulation();
-        void launch_simulation();
-        Physics::Time get_simulation_time();
+private:
+    Network * get_network();
+    Physics::Time get_simulation_time();
 
-    private:
-        Network * get_network();
+private:
+    Network network_;
+    Physics::Time time_of_simulation_;
+    Physics::Time time_step_;
+    Physics::Time spike_interval_;
 
-    private:
-        Network network_;
-        Physics::Time time_of_simulation_;
-        Physics::Time time_step_;
-        Physics::Time spike_interval_;
+private:
+    FRIEND_TEST(AnalyticNeuronTests, TestSingleNeuronSimulation);
+    FRIEND_TEST(AnalyticNeuronTests, TestDecayNeuronVoltage);
 
-    private:
-        FRIEND_TEST(AnalyticNeuronTests, TestSingleNeuronSimulation);
-        FRIEND_TEST(AnalyticNeuronTests, TestDecayNeuronVoltage);
-        FRIEND_TEST(ExplicitNeuronTests, TestSingleNeuronSimulation);
-        FRIEND_TEST(ExplicitNeuronTests, TestDecayNeuronVoltage);
-        FRIEND_TEST(ImplicitNeuronTests, TestSingleNeuronSimulation);
-        FRIEND_TEST(ImplicitNeuronTests, TestDecayNeuronVoltage);
+    FRIEND_TEST(ExplicitNeuronTests, TestDecayNeuronVoltage);
+
+    FRIEND_TEST(ImplicitNeuronTests, TestDecayNeuronVoltage);
 };
 
 #endif // SIMULATION_HPP

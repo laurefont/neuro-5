a24b2d588d51d0a110ab1fdb755990433f8bb5ed
Léo Sumi <leo.sumi@epfl.ch>
2016-12-16 14:34:38 +0100

    doc for simulation class

--- include/Simulation.hpp
+++ include/Simulation.hpp
@@ -6,14 +6,16 @@
 #include <Physics.hpp>
 #include <Network.hpp>
 
-/**
- * @brief The main class of the simulation
- *
- * Make the simulation of the network and draw the results plot.
- */
+///
+/// @brief The main class of the simulation
+///
+/// Make the simulation of the network.
+///
 class Simulation
 {
 public:
+    ///
+    /// Constructor
     ///
     /// Create a fixed-step method simulation
     ///
@@ -21,9 +23,19 @@ public:
     /// @param time_of_simulation duration of the simulation
     /// @param time_step time step duration
     /// @param type type of solution
+    /// @param add_external_current presence of external current arriving
+    /// @param neuron_csv_files neurons that will output a file
+    /// @param firing_threshold firing threshold
+    /// @param refractory_period refractory period duration
+    /// @param resting_potential resting potential at equilibrium state
+    /// @param reset_potential reset potential after a neuron has fired
+    /// @param transmission_delay duration of a spike transmission
+    /// @param tau neuron membrane time constant
     /// @param gamma proportion of inhibitory neurons over excitatory neurons
     /// @param epsilon connections density
     /// @param external_factor external factor
+    /// @param random_seed seed for the random generators
+    /// @param spike_interval spike counting time interval for output files
     ///
     Simulation( unsigned int const number_neurons,
                 Physics::Time const& time_of_simulation,
@@ -44,13 +56,25 @@ public:
                 Physics::Time spike_interval = SPIKE_INTERVAL );
 
     ///
+    /// Constructor
+    ///
     /// Create a simulation with an analytic variable-step solution
     ///
     /// @param number_neurons number of neurons in the network
     /// @param time_of_simulation duration of the simulation
+    /// @param add_external_current presence of external current arriving
+    /// @param neuron_csv_files neurons that will output a file
+    /// @param firing_threshold firing threshold
+    /// @param refractory_period refractory period duration
+    /// @param resting_potential resting potential at equilibrium state
+    /// @param reset_potential reset potential after a neuron has fired
+    /// @param transmission_delay duration of a spike transmission
+    /// @param tau neuron membrane time constant
     /// @param gamma proportion of inhibitory neurons over excitatory neurons
     /// @param epsilon connections density
     /// @param external_factor external factor
+    /// @param random_seed seed for the random generators
+    /// @param spike_interval spike counting time interval for output files
     ///
     Simulation( unsigned int const number_neurons,
                 Physics::Time const& time_of_simulation,
@@ -68,11 +92,33 @@ public:
                 unsigned random_seed = RANDOM_SEED,
                 Physics::Time spike_interval = SPIKE_INTERVAL );
 
-    virtual ~Simulation();
+    ///
+    /// @brief Destructor
+    ///
+    ~Simulation();
+
+    ///
+    /// @brief launch the simulation
+    ///
     void launch_simulation();
 
 private:
+    ///
+    /// @brief Get the neurons network of the current simulation
+    ///
+    /// Used for test
+    ///
+    /// @return pointer to the network attribut
+    ///
     Network * get_network();
+
+    ///
+    /// @brief Get the current simulation time
+    ///
+    /// Used for test
+    ///
+    /// @return the current simulation time
+    ///
     Physics::Time get_simulation_time();
 
 private:
